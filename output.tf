e51925434281d9156d70e36e935c58298707bb7e
unknown <prathmeshdahikar123@gmail.com>
2026-02-20 15:45:27 +0530

    change the output file

--- output.tf
+++ output.tf
@@ -1,16 +1,15 @@
 output "cluster_id" {
-  value = aws_eks_cluster.devopsshack.id
+  value = aws_eks_cluster.devopsshack1.id  # Added the "1"
 }
 
 output "node_group_id" {
-  value = aws_eks_node_group.devopsshack.id
+  value = aws_eks_node_group.devopsshack1_node_group.id  # Full resource name
 }
 
 output "vpc_id" {
-  value = aws_vpc.devopsshack_vpc.id
+  value = aws_vpc.devopsshack1_vpc.id  # Added the "1"
 }
 
 output "subnet_ids" {
-  value = aws_subnet.devopsshack_subnet[*].id
+  value = aws_subnet.devopsshack1_subnet[*].id  # Added the "1"
 }
-
